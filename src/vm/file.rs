d6987c4ffecc0c70db6874ee407b0efe3dee32bb
monochrome <sisshiki@mac.com>
2020-02-15 19:28:31 +0900

    Support File#basename, extname, binread

--- src/vm/file.rs
+++ src/vm/file.rs
@@ -1,4 +1,6 @@
 use crate::vm::*;
+use std::fs::File;
+use std::io::Read;
 use std::path::*;
 
 pub fn init_file(globals: &mut Globals) -> Value {
@@ -6,21 +8,27 @@ pub fn init_file(globals: &mut Globals) -> Value {
     let class = ClassRef::from(id, globals.object);
     let obj = Value::class(globals, class);
     globals.add_builtin_class_method(obj, "join", join);
+    globals.add_builtin_class_method(obj, "basename", basename);
+    globals.add_builtin_class_method(obj, "extname", extname);
+    globals.add_builtin_class_method(obj, "binread", binread);
     obj
+}
+
+// Utils
+
+fn string_to_path(vm: &mut VM, string: Value) -> Result<PathBuf, RubyError> {
+    match string.as_string() {
+        Some(file) => Ok(PathBuf::from(file)),
+        None => Err(vm.error_type("Arg must be String.")),
+    }
 }
 
 // Class methods
 
 fn join(vm: &mut VM, args: &Args, _block: Option<MethodRef>) -> VMResult {
     vm.check_args_num(args.len(), 2, 2)?;
-    let mut path = PathBuf::from(match args[0].as_string() {
-        Some(s) => s,
-        None => return Err(vm.error_type("Arguments ust be String.")),
-    });
-    let arg = PathBuf::from(match args[1].as_string() {
-        Some(s) => s,
-        None => return Err(vm.error_type("Arguments ust be String.")),
-    });
+    let mut path = string_to_path(vm, args[0])?;
+    let arg = string_to_path(vm, args[1])?;
 
     for p in arg.iter() {
         if p == ".." {
@@ -30,4 +38,47 @@ fn join(vm: &mut VM, args: &Args, _block: Option<MethodRef>) -> VMResult {
         }
     }
     Ok(Value::string(path.to_string_lossy().to_string()))
+}
+
+fn basename(vm: &mut VM, args: &Args, _block: Option<MethodRef>) -> VMResult {
+    let len = args.len();
+    vm.check_args_num(len, 1, 1)?;
+    let filename = string_to_path(vm, args[0])?;
+    let basename = match filename.file_name() {
+        Some(ostr) => Value::string(ostr.to_string_lossy().into_owned()),
+        None => Value::nil(),
+    };
+    Ok(basename)
+}
+
+fn extname(vm: &mut VM, args: &Args, _block: Option<MethodRef>) -> VMResult {
+    let len = args.len();
+    vm.check_args_num(len, 1, 1)?;
+    let filename = string_to_path(vm, args[0])?;
+    let extname = match filename.extension() {
+        Some(ostr) => format!(".{}", ostr.to_string_lossy().into_owned()),
+        None => "".to_string(),
+    };
+    Ok(Value::string(extname))
+}
+
+fn binread(vm: &mut VM, args: &Args, _block: Option<MethodRef>) -> VMResult {
+    let len = args.len();
+    vm.check_args_num(len, 1, 1)?;
+    let filename = match string_to_path(vm, args[0])?.canonicalize() {
+        Ok(file) => file,
+        Err(_) => {
+            return Err(vm.error_argument(format!("Invalid filename. {}", vm.val_pp(args[0]))))
+        }
+    };
+    let mut file = match File::open(&filename) {
+        Ok(file) => file,
+        Err(_) => return Err(vm.error_internal(format!("Can not open file. {:?}", &filename))),
+    };
+    let mut contents = String::new();
+    match file.read_to_string(&mut contents) {
+        Ok(file) => file,
+        Err(_) => return Err(vm.error_internal("File content is invalid as UTF8.")),
+    };
+    Ok(Value::string(contents))
 }
