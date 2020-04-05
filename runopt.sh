65ec5d3af878b26447b272d197cdacff2d018bb4
monochrome <sisshiki@mac.com>
2020-04-05 22:37:31 +0900

    fix.

--- runopt.sh
+++ runopt.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -x
 cargo build --release
-ruby ../optcarrot/bin/optcarrot-bench --load-ppu=../optcarrot/ppu.rb -b
+#ruby ../optcarrot/bin/optcarrot-bench --load-ppu=../optcarrot/ppu.rb -b
+ruby ../optcarrot/bin/optcarrot-bench -b
 #target/release/ruruby ../optcarrot/bin/optcarrot-bench --load-ppu=../optcarrot/ppu.rb -b
 target/release/ruruby ../optcarrot/bin/optcarrot-bench -b
