eb57521b3fca18c8bb23fb6e0721b79fb8f2d1fb
monochrome <sisshiki@mac.com>
2020-07-07 08:23:19 +0900

    Fix.

--- src/test.rs
+++ src/test.rs
@@ -1,10 +1,26 @@
+#[cfg(feature = "perf")]
+use crate::vm::perf::*;
 use crate::*;
 use std::path::PathBuf;
 
 pub fn eval_script(script: impl Into<String>, expected: Value) {
     let mut globals = GlobalsRef::new_globals();
     let mut vm = globals.new_vm();
-    match vm.run(PathBuf::from(""), &script.into(), None) {
+    let res = vm.run(PathBuf::from(""), &script.into(), None);
+    #[cfg(feature = "perf")]
+    {
+        let mut perf = Perf::new();
+        let globals = vm.globals;
+        for vm in &globals.fibers {
+            perf.add(&vm.perf);
+        }
+        perf.print_perf();
+    }
+    #[cfg(feature = "gc-debug")]
+    {
+        ALLOC.with(|a| a.borrow_mut().as_ref().unwrap().print_mark());
+    }
+    match res {
         Ok(res) => {
             if res != expected {
                 panic!("Expected:{:?} Got:{:?}", expected, res);
@@ -21,7 +37,21 @@ pub fn eval_script(script: impl Into<String>, expected: Value) {
 pub fn assert_script(script: impl Into<String>) {
     let mut globals = GlobalsRef::new_globals();
     let mut vm = globals.new_vm();
-    match vm.run(PathBuf::from(""), &script.into(), None) {
+    let res = vm.run(PathBuf::from(""), &script.into(), None);
+    #[cfg(feature = "perf")]
+    {
+        let mut perf = Perf::new();
+        let globals = vm.globals;
+        for vm in &globals.fibers {
+            perf.add(&vm.perf);
+        }
+        perf.print_perf();
+    }
+    #[cfg(feature = "gc-debug")]
+    {
+        ALLOC.with(|a| a.borrow_mut().as_ref().unwrap().print_mark());
+    }
+    match res {
         Ok(_) => {}
         Err(err) => {
             err.show_err();
@@ -35,7 +65,21 @@ pub fn assert_error(script: impl Into<String>) {
     let mut globals = GlobalsRef::new_globals();
     let mut vm = globals.new_vm();
     let program = script.into();
-    match vm.run(PathBuf::from(""), &program, None) {
+    let res = vm.run(PathBuf::from(""), &program, None);
+    #[cfg(feature = "perf")]
+    {
+        let mut perf = Perf::new();
+        let globals = vm.globals;
+        for vm in &globals.fibers {
+            perf.add(&vm.perf);
+        }
+        perf.print_perf();
+    }
+    #[cfg(feature = "gc-debug")]
+    {
+        ALLOC.with(|a| a.borrow_mut().as_ref().unwrap().print_mark());
+    }
+    match res {
         Ok(_) => panic!("Must be an error:{}", program),
         Err(err) => {
             err.show_err();
