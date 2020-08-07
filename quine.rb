6f5b17ecebb7cc1c67e80910f0924d247a72162f
sisshiki1969 <sisshiki@mac.com>
2020-08-07 22:43:42 +0900

    Refactor.

---
+++ quine.rb
@@ -0,0 +1,22 @@
+    n=2;eval$s=%q{Z=?\s;eval"$><<S=Z*4"+(%w{+"n=#{-~n%3};eval$s=%q{#$s}#YE";$>.isat ty&&
+   (r="\e[43;3#{C="#{n*5%9+1}m"}#{T}\e[4"+C+S[1568,79]+E="\e[0m";r[81,21]="\e[37m# {(["Ca
+  f\u00e9_au_lait","Yogurt","Fruit_mix"][n].chars*Z).tr(?_,"").center(21)}\e[3"+C ;a=%~POS
+  A[`ER]`PASX1cTc22V6NNP.QOYGMXXIG7KK:bCCaVN8WZ[]UQMMS`cBFFJJHHY`QTUIUURRPTOcRV_a LLUT`WXL
+ W]a_c_bV`XXYa_9}+[T='  B  A  L  A  N  C  E  D   F  O  O  D  ']*0+%w{bZZYb_][9cc ????`9^acG
+ G,,N9DU`DKcUKU3K4!4!4!QXTSSS""9`9`#U`KcK--S;;/GOT<QE$U=>F==Q0@%U/P/B=S0Q`PM&XVV V15CMRHMSH
+RKO>==QMQVR    'b`&DK>BS<XE$T>T33DDDUM<V@@E(((TCT0A<0A"')5CXPcQa54X@@Y#KcK--S;; /GOT<Q`$)T)T
+:a4A%%#X   VS6a   '   b`&DK>BS<T7**]  ^^b6+++]~;   P=Str    uct.new(:x,:d,:p,:v );M=(-5**7..
+b=0).m   ap{[]};A=s  =[];t=Time.now;q=?y.succ;(    s=S     .scan(/.+/ );M[0]<<P [25i-b%3*5i-
+9,0,0  ,2+1  i] ;6  0.t   ime  s  {  |i    |j=  i  %2  0  ;i<  40 ?    [    M[j -1],m=M[j],M
+[j+1  ]].  ea  ch  {|  n|  m.   ea  ch  {|  p| n. ea ch  {|  q|  d=p .x  -q  .x ;w=d.abs-4;w
+<0&   &(  i<2 0?  p.  d+=  w  *w:  p.     p+=  w    *(   d  *(3 -p. d-     q.d) +(p.v-q.v)*4
+)/p   .d  )}  }   }   :M  .  shif  t   .ea c  h{   |p|  y,  x=  (   p.  x+= p.v +=p.p/10).re
+ct;   p.p   =  [4  3-   b/9 .0-y,1  ].    m  in- [x,p  .d=0   ,  x   -9    2].s ort[1]*2i;p.
+v/=[   1,p.v.a   bs/2].max;M[20-j+[0  ,(x+  4).div(5   ),19].sort[1]]<<p;35.tim es{|w|v=x.to
+_i-3+w         %7;c=s[w=y.div(2)-2+        w/7];(x-v)**2+(y-w*2)**2<16&&0<=w&&c &&(k=(w*2-21
+ )**2/99)<=v&&c[v]&&k+79!=v&&c[v]=q}}};(24-b/18..21).map{|k|s[k]=Z*(k=(k*2-21)** 2/99)+q*79
+ +Z+q*2*(6-k)};s*="\e[B\r";"  Your favorite flavor  ";b+=1;A<<"\e[A\r"*21+s.gsub (/\172+/){
+  "\e[43m"+$&.tr(q,Z)+E})while+s.count(q)<1950;A.map{|q|sleep([t-Time.now+3,2e-2] .max);$>
+  <<s=q};$><<s.gsub(?m,";33m").gsub(Z){S.slice!(/./)};b=?]*33.upto(91){|i|a=~/../ ;a=$'.gs
+   ub(i.chr,$&)}*2;Z<<8;(b+a.gsub(?^,"^]"*41)+b).bytes{|c|c-=86;c<8?sleep(3e-2):$> <<(c<(
+    'CalorieMate-Liquid-Quine';9)?r.slice!(/\e.*?m|./):c>9?"\e[%X"%c:Z)});puts})*"" }#YE
