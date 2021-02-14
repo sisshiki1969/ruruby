215a9fe986f3dd4acb0b8645087d44bb468a3c47
sisshiki1969 <sisshiki@mac.com>
2021-02-14 10:37:51 +0900

    Fix

--- test.rb
+++ test.rb
@@ -1,3 +1,13 @@
 a = []
-10000000.times { |i| a << i }
-a.each{ |x| x }
+10.times { |i| a << i }
+a.each { |e| 
+    puts e
+    if e == 3
+        class Array
+            def each
+                3
+            end
+        end
+    end
+}
+puts a.each
