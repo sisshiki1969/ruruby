2f7ad8febad54c554327441456e532b0bf4c297d
sisshiki1969 <sisshiki@mac.com>
2021-02-19 22:21:04 +0900

    Add test.

---
+++ tests/ivar_get.rb
@@ -0,0 +1,31 @@
+class C
+    def fn
+        i = 0
+        while i < 10000000 do
+            @a
+            @a
+            @a
+            @a
+            @a
+            @a
+            @a
+            @a
+            @a
+            @a
+            @a
+            @a
+            @a
+            @a
+            @a
+            @a
+            @a
+            @a
+            @a
+            @a
+            i += 1
+        end
+    end
+end
+
+o = C.new
+o.fn
