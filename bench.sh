2d49943b5dbc9d40bf93374e21b47ef0edb32062
sisshiki1969 <sisshiki@mac.com>
2021-11-24 22:46:16 +0900

    Fix bench.sh

--- bench.sh
+++ bench.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cargo build --release
-ver='3.0.1'
-benchmark-driver bench/benchmark/* -rbenv $ver -e 'target/release/ruruby' --output simple
+ver='3.0.0'
+benchmark-driver bench/benchmark/* --rbenv $ver -e 'target/release/ruruby' --output simple
